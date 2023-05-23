ee16f090b7dad472638ba57b71b2d587a967eb9b
Матвей <roganov94@mail.ru>
2023-05-23 16:42:41 +0300

    Реализован класс Chanell и метод print_info

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,10 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+os.environ['YT_API_KEY'] = 'AIzaSyDmvodVAPDoWVMQhSDRvRWZtDEgC2Joijw'
 
 
 class Channel:
@@ -5,8 +12,16 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        self.api_key: str = os.getenv('YT_API_KEY')
+
+        # создать специальный объект для работы с API
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
